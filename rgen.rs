e5901b52547c3b79bed94fc0b014dddc06a14304
Matt Irvine <mji7wb@virginia.edu>
2014-05-18 18:53:05 -0400

    Now loads templates, content is a work in progress.

--- rgen.rs
+++ rgen.rs
@@ -68,7 +68,7 @@ fn main() {
 	let vars: Vec<(~str,~str)> = loadVars(pathToInclude);
 
 	//Load internal link names so that they can be replaced for includes. Matched with $link or {$link}
-	let internalLinks: Vec<(~str,~str)> = loadLinks(contentFiles);
+	let internalLinks: Vec<(~str,~str)> = loadLinks(&contentFiles);
 
 	//Process includes. Matched with {.include}
 	let includes: Vec<(~str,~str)> = loadIncludes(includeFiles, &vars, &internalLinks);
@@ -76,8 +76,16 @@ fn main() {
 	//Process global css/js
 	let globalCSSJS: Vec<~str> = loadGlobalCSSJS(pathToTemplates);
 
-	//Buffer template names first, then process
-	let templates: Vec<Template> = loadTemplates(templateFiles, &vars, &internalLinks, &includes);
+	//Load templates
+	let mut templatesPre: Vec<Template> = loadTemplates(templateFiles, &vars, &internalLinks, &includes);
+
+	//Process template inheritance
+	let templates: Vec<Template> = processInheritance(&mut templatesPre);
+
+	//Load content
+	let content: Vec<Page> = loadContent(contentFiles, &vars, &internalLinks, &includes);
+
+	//Process content
 
 	//Generate content
 }
@@ -116,7 +124,7 @@ fn loadIncludes(includeFiles: Vec<Path>, vars: &Vec<(~str,~str)>, internalLinks:
 }
 
 //This method is slow. Fix it. 
-fn loadLinks(contentFiles: Vec<Path>) -> Vec<(~str,~str)> {
+fn loadLinks(contentFiles: &Vec<Path>) -> Vec<(~str,~str)> {
 	let mut returnVec: Vec<(~str,~str)> = Vec::new();
 	for p in contentFiles.iter() {
 		let mut fileReader = BufferedReader::new(File::open(p));
@@ -185,22 +193,131 @@ fn loadGlobalCSSJS(pathToTemplates: Path) -> Vec<~str> {
 }
 
 struct Template {
+	name: ~str,
+	inherit: ~str,
 	headData: Vec<~str>,
 	blockTemplates: Vec<(~str,~str)>,
 	content: ~str
 }
 
-enum TemplateStep {
-	incCSS,
-	incJS,
-	blocks,
-	content
+impl Clone for Template {
+	fn clone(&self) -> Template {
+		let myTemplate = Template {name: self.name.clone(), inherit: self.inherit.clone(), headData: self.headData.clone(), blockTemplates: self.blockTemplates.clone(), content: self.content.clone()};
+		return myTemplate;
+	}
 }
 
+enum TemplateStep {
+	InInherit,
+	InCSS,
+	InJS,
+	InBlocks,
+	InContent
+}
+
+//This hasn't been tested so if something is going wrong it's probably here.
 fn loadTemplates(templateFiles: Vec<Path>, vars: &Vec<(~str,~str)>, internalLinks: &Vec<(~str,~str)>, includes: &Vec<(~str,~str)>) -> Vec<Template> {
 	let mut returnVec: Vec<Template> = Vec::new();
-	//Do stuff here
-
+	for file in templateFiles.iter() {
+		let mut myTemplate = Template {name: file.filestem_str().unwrap().to_owned(), inherit: "".to_owned(), headData: Vec::new(), blockTemplates: Vec::new(), content: "".to_owned()};
+		let mut fileReader = BufferedReader::new(File::open(file));
+		let mut curLine = fileReader.read_line().unwrap().trim().to_owned();
+		let mut curBlockName = "".to_owned();
+		let mut curBlockContent = "".to_owned();
+		let mut myStep: TemplateStep = InContent;
+		if curLine.starts_with("inherit ") {
+			myStep = InInherit;
+			let inheritance = curLine.split_str(" ").last().unwrap();
+			myTemplate.inherit = inheritance.to_owned();
+		}
+		else if curLine.starts_with("css") {
+			myStep = InCSS;
+		}
+		else if curLine.starts_with("js") {
+			myStep = InJS;
+		}
+		else if curLine.starts_with("blocks") {
+			myStep = InBlocks;
+		}
+		loop {
+			let nextLine = fileReader.read_line();
+			match nextLine {
+				Ok(tex) => { curLine = tex.to_owned() },
+				Err(_) => { break }
+			}
+			let mut advanced = false;
+			match curLine.trim() {
+				"css" => {
+					myStep = InCSS;
+					advanced = true;
+				},
+				"js" => {
+					myStep = InJS;
+					advanced = true;
+				},
+				"blocks" => {
+					myStep = InBlocks;
+					advanced = true;
+				},
+				"" => {
+					advanced = true; //Either we advance or this line is blank so ignore it regardless
+					match myStep {
+						InBlocks => { myStep = InContent; advanced = true; },
+						_ => { }
+					}
+				},
+				_ => { }
+			}
+			if !advanced {
+				let curLineUnTrimmed = curLine.to_owned();
+				curLine = curLine.trim().to_owned();
+				//Replace variables/links/includes if we might need to.
+				if curLine.contains_char('{') && curLine.contains_char('}') {
+					//Check if there are variables/links and replace them.
+					if curLine.contains("{$") || curLine.contains("{%") {
+						curLine = replaceVars(curLine, vars, internalLinks);
+					}
+					//Check if there are includes and insert them.
+					if curLine.contains("{.") {
+						curLine = insertIncludes(curLine, includes);
+					}
+				}
+				match myStep {
+					InInherit => { }, //Inheritance was taken care of by the first line so do nothing here.
+					InCSS => {
+						myTemplate.headData.push("<link rel='stylesheet' type='text/css' href='resources/css/" + curLine + "'>");
+					},
+					InJS => {
+						myTemplate.headData.push("<script type='text/javascript' src='resources/js/" + curLine + "'></script>");
+					},
+					InBlocks => {
+						//Support double tab or 8 spaces. This is not very flexible. 
+						if curLineUnTrimmed.starts_with("\t\t") || curLineUnTrimmed.starts_with("        ") {
+							//In a block
+							curBlockContent = curBlockContent + "\n" + curLine;
+						}
+						else {
+							//Found a new block
+							let curBlock: (~str,~str) = (curBlockName.clone(), curBlockContent.clone());
+							myTemplate.blockTemplates.push(curBlock);
+							curBlockName = "".to_owned();
+							curBlockContent = "".to_owned();
+						}
+					},
+					InContent => {
+						myTemplate.content = curLine;
+						match fileReader.read_to_str() {
+							Ok(tex) => {
+								myTemplate.content = myTemplate.content + "\n" + tex;
+							},
+							Err(_) => {}
+						}
+					}
+				}
+			}
+		}
+		returnVec.push(myTemplate);
+	}
 	return returnVec;
 }
 
@@ -215,3 +332,62 @@ fn replaceVars(mut text: ~str, vars: &Vec<(~str,~str)>, internalLinks: &Vec<(~st
 	}
 	return text
 }
+
+fn insertIncludes(mut text: ~str, includes: &Vec<(~str,~str)>) -> ~str {
+	for include in includes.iter() {
+		let (ref a, ref b) = *include;
+		text = text.replace("{." + *a + "}", *b);
+	}
+	return text;
+}
+
+//It's probably possible to clean this up a little bit. 
+fn processInheritance(templatesPre: &mut Vec<Template>) -> Vec<Template> {
+	let mut templates: Vec<Template> = Vec::new();
+	let oldTemplates = templatesPre.clone();
+	for template in templatesPre.mut_iter() {
+		if template.inherit != "".to_owned() {
+			for template2 in oldTemplates.iter() {
+				if template2.name == template.inherit {
+					template.headData.push_all(template2.headData.as_slice());
+					template.blockTemplates.push_all(template2.blockTemplates.as_slice());
+					template.content = template2.content.replace("{content}", template.content);
+				}
+			}
+		}
+		templates.push(template.clone());
+	}
+	return templates;
+}
+
+struct Page {
+	path: ~str,
+	linkName: ~str,
+	title: ~str,
+	template: ~str,
+	blocks: Vec<Block>,
+	content: ~str
+}
+
+struct Block {
+	name: ~str,
+	content: Vec<(~str,~str)> //Vector of tuples: (variable, content)
+}
+
+enum ContentStep {
+	CInConfig,
+	CInCSS,
+	CInJS,
+	CInBlocks,
+	CInContent
+}
+
+//This one's a work in progress.
+fn loadContent(contentFiles: Vec<Path>, vars: &Vec<(~str,~str)>, internalLinks: &Vec<(~str,~str)>, includes: &Vec<(~str,~str)>) -> Vec<Page> {
+	let mut pages: Vec<Page> = Vec::new();
+	for file in contentFiles.iter() {
+		let mut myPage = Page {path: "".to_owned(), linkName: "".to_owned(), title: "".to_owned(), template: "".to_owned(), blocks: Vec::new(), content: "".to_owned()};
+	}
+	return pages;
+}
+
